3b81ddcbc48ba3dcfb32a4d733b82de0bf199809
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-03 15:47:12 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,11 @@
 def my_select(collection)
   if block_given?
-    
     i = 0
     while i<collection.length
-      
-
+      yield collection[i] if true 
+      i += 1
+    end
+  else
+    nil
+  end
 end
