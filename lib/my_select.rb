cd25d4b9d2f94f074287b4d1f9f1d9c30a79a583
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-03 15:50:55 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
     selected_elements = []
     i = 0
     while i<collection.length
-      if yield collection[i] == true
+      if (yield collection[i] == true)
         selected_elements << collection[i]
       end 
       i += 1
